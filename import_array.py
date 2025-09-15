f5d12aeb7f2b26b0f682105aa569748f335da942
kryssie3290 <christabeladamu2@gmail.com>
2025-09-15 04:41:02 -0700

    import_array.py

---
+++ import_array.py
@@ -0,0 +1,9 @@
+from my_array import Array
+fruits = Array()
+fruits.append("mango")
+fruits.append("apple")
+fruits.insert(0, "plantain")
+fruits.pop()
+print(fruits)
+#fruits.remove("plantain")
+#3print(fruits)
